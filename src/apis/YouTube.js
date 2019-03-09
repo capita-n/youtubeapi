fda97eca3713586b8858ef91283fe19ced8cf28a
nath <nhatty94@gmail.com>
2019-03-09 23:18:39 +0100

    initial commit

---
+++ src/apis/YouTube.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const KEY = 'AIzaSyAJy3RsMx1Q3P5zA6KIo95ID181p92Zuq8'
+
+export default axios.create({
+    baseURL :'https://www.googleapis.com/youtube/v3',
+    params:{
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
